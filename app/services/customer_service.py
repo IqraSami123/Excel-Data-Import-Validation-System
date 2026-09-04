620a5bb982ab780789904d05033c7b4445996673
IqraSami123 <iqrasmi998@gmail.com>
2026-09-04 14:02:53 +0500

    customer CRUD completed

--- app/services/customer_service.py
+++ app/services/customer_service.py
@@ -1,8 +1,12 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func, select
 from app.models.customer import Customer
-from app.schemas.customer import CustomerCreate
+from app.schemas.customer import CustomerCreate, CustomerUpdate
 
+
+################################################################
+#_______________________create customer_________________________
+################################################################
 
 def create_customer(
     db: Session,
@@ -24,6 +28,10 @@ def create_customer(
 
     return customer
 
+
+################################################################
+#_______________________get customers list______________________
+################################################################
 
 def get_customers(
     db: Session,
@@ -54,5 +62,80 @@ def get_customers(
     }
 
 
+################################################################
+#_______________________get customer by id______________________
+################################################################
+
 def get_customer(db: Session, customer_id: int) -> Customer | None:
     return db.get(Customer, customer_id)
+
+
+################################################################
+#_______________________completly update customer_______________
+################################################################
+
+def update_customer(
+    db: Session,
+    customer_id: int,
+    customer_data: CustomerCreate,
+) -> Customer | None:
+
+    customer = db.get(Customer, customer_id)
+
+    if customer is None:
+        return None
+
+    customer.name = customer_data.name
+    customer.email = customer_data.email
+    customer.phone = customer_data.phone
+    customer.age = customer_data.age
+    customer.city = customer_data.city
+    customer.status = customer_data.status
+
+    db.commit()
+    db.refresh(customer)
+
+    return customer
+
+
+
+################################################################
+#_______________________partialy update customer________________
+################################################################
+
+def partial_update_customer(
+    db: Session,
+    customer_id: int,
+    customer_data: CustomerUpdate,
+) -> Customer | None:
+
+    customer = db.get(Customer, customer_id)
+
+    if customer is None:
+        return None
+
+    update_data = customer_data.model_dump(exclude_unset=True)   # this line extracts the fields that have been provided in the update request, ignoring any fields that were not included. This allows for partial updates.
+
+    for field, value in update_data.items():
+        setattr(customer, field, value)
+
+    db.commit()
+    db.refresh(customer)
+
+    return customer
+
+
+################################################################
+#_______________________delete customer________________________
+################################################################
+
+def delete_customer(db: Session, customer_id: int) -> Customer | None:
+    customer = db.get(Customer, customer_id)
+
+    if customer is None:
+        return None
+
+    db.delete(customer)
+    db.commit()
+
+    return customer
